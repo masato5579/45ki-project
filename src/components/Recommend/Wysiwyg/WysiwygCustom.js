8cca1369f5639e0e64e9d87c110378a80944aba0
masato <namonakisizin@gmail.com>
2021-02-23 21:55:10 +0900

    Wysiwyg firebase同期(欠陥あり)

--- src/components/Recommend/Wysiwyg/WysiwygCustom.js
+++ src/components/Recommend/Wysiwyg/WysiwygCustom.js
@@ -1,4 +1,8 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useContext, useEffect } from "react";
+import { convertToRaw, convertFromRaw, EditorState } from "draft-js";
+
+import firebase from "../../../Firebase/firebase";
+import { AuthContext } from "../../../Route/AuthService";
 
 import Editor, { createEditorStateWithText } from "@draft-js-plugins/editor";
 
@@ -10,6 +14,9 @@ import {
   BoldButton,
   UnderlineButton,
   CodeButton,
+  HeadlineOneButton,
+  HeadlineTwoButton,
+  HeadlineThreeButton,
   UnorderedListButton,
   OrderedListButton,
   BlockquoteButton,
@@ -37,6 +44,8 @@ const text =
 const WysiwygCustom = () => {
   const editor = useRef();
 
+  const user = useContext(AuthContext);
+
   const [editorState, setEditorState] = useState(
     createEditorStateWithText(text)
   );
@@ -45,14 +54,39 @@ const WysiwygCustom = () => {
     createEditorStateWithText(text)
   );
 
+  const [edit, setEdit] = useState("");
   const onChange = (editorState) => {
     setEditorState(editorState);
     setEditorStateImg(editorState);
   };
 
-  const onSave = () => {
-    // const editString = document.querySelector(".edit").outerHTML;
-    // setEdit(editString);
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection("recommend")
+      .orderBy("dates", "desc")
+      .onSnapshot((snapshot) => {
+        const rec = snapshot.docs.map((doc) => {
+          return doc.data();
+        });
+        console.log(rec[0].edit);
+        setEdit(rec[0].edit);
+      });
+  }, []);
+
+  const onSave = (e) => {
+    e.preventDefault();
+    const edit = document.querySelector(".edit");
+    const edit2 = edit.outerHTML; //htmlを文字列にする
+    setEdit(edit2);
+
+    firebase
+      .firestore()
+      .collection("recommend")
+      .add({
+        edit: edit2,
+        dates: String(new Date()),
+      });
   };
 
   const focus = () => {
@@ -62,7 +96,7 @@ const WysiwygCustom = () => {
   return (
     <Container>
       <h1>おすすめ</h1>
-      <div className="editor" onClick={focus}>
+      <div className="editor edit" onClick={focus}>
         <Editor
           editorState={editorState}
           onChange={onChange}
@@ -78,6 +112,9 @@ const WysiwygCustom = () => {
               <BoldButton {...externalProps} />
               <UnderlineButton {...externalProps} />
               <CodeButton {...externalProps} />
+              <HeadlineOneButton {...externalProps} />
+              <HeadlineTwoButton {...externalProps} />
+              <HeadlineThreeButton {...externalProps} />
               <Separator {...externalProps} />
               <UnorderedListButton {...externalProps} />
               <OrderedListButton {...externalProps} />
@@ -99,6 +136,7 @@ const WysiwygCustom = () => {
       <div>
         <button onClick={onSave}>保存</button>
       </div>
+      <div dangerouslySetInnerHTML={{ __html: edit }}></div>
     </Container>
   );
 };
